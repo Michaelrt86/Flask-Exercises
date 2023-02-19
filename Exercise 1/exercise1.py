b03e4f21690784c65dec7f14e452b01d5453b8e6
Michaelrt86 <Michaelrt867@gmail.com>
2023-02-18 20:54:59 -0500

    Exercise1 almost completed

---
+++ Exercise 1/exercise1.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/get')
+def testMethod():
+    TodayDate = datetime.datetime.now()
+    thisYear = TodayDate.strftime("%Y")
+    thisMonth = TodayDate.strftime("%B")
+    thisWeekDay = TodayDate.strftime("%A")
+    thisDay = TodayDate.strftime("%d")
+    date = 'The current date time is ' + thisWeekDay + ', ' + thisMonth + ' ' + thisDay + ' ' + thisYear
+    return render_template('get.html', htmlDate = date)
+
+
+if __name__ == '__main__':
+    app.run()
