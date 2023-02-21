05acbb556f9a3d11d2642783f946f921d872a9bd
Michaelrt86 <Michaelrt867@gmail.com>
2023-02-21 14:08:50 -0500

    Exercise 2 Completed!

--- Exercise 2/exercise2.py
+++ Exercise 2/exercise2.py
@@ -1,25 +1,23 @@
 from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
-
-@app.route("/", methods=["POST", "GET"])
+#Zoey Vail helped me on this assignment
+@app.route("/")
 def submit():
-    if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("user", usr = user))
-    else:
         return render_template('form.html')
-@app.route("/<usr>")
-def user(usr):
-    try:
-        userResponse = int(usr)
-        if userResponse % 2 == 0:
-            return render_template("calculate.html", number = userResponse,state = "even")
-        else:
-            return f"<h1>The number is odd</h1>"
-    except Exception:
-        return render_template("form.html")
+@app.route("/calculate",methods=["POST"])
+def calculate():
+    if request.method == 'POST':
+        try:
+            userResponse = int(request.form.get('number'))
+            if userResponse % 2 == 0:
+                temp = str(userResponse) + ' is even'
+            else:
+                temp = str(userResponse) + ' is even'
+        except Exception:
+            temp = 'Invalid input'
+        return render_template('calculate.html', temp2 = temp)
     
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
